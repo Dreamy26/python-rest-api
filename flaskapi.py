9f4b5bd3f73244826c4728e092e22777e6c847aa
Dreamy <dreamy26@icloud.com>
2022-11-25 16:40:29 -0500

    build: Adds post to flask

--- flaskapi.py
+++ flaskapi.py
@@ -1,12 +1,20 @@
+    
 from flask import Flask, json
 
+# two company objects
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
+# initialize Flask
 api = Flask(__name__)
 
 @api.route('/companies', methods=['GET'])
-def get_companies():
-    return json.dumps(companies)
+def get_companies(): # GET request, two companies will be returned
+  return json.dumps(companies)
+
+# Add another company using the POST method
+@api.route('/companies', methods=['POST'])
+def post_companies():
+    return json.dumps({"success": True}), 201
 
 if __name__ == '__main__':
     api.run()
