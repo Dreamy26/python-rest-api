ab8817108dd881ea1888df8d59760de562ffda0f
Dreamy <dreamy26@icloud.com>
2022-11-25 21:09:18 -0500

    build: Adds additional endpoint to FalconAPI

--- falconapi.py
+++ falconapi.py
@@ -2,8 +2,16 @@ import falcon, json
 
 class CompaniesResource(object):
   companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+
+# falcon uses separate classes for each endpoint
   def on_get(self, req, resp):
     resp.body = json.dumps(self.companies)
+    
+  def on_post(self, req, resp):
+    resp.status = falcon.HTTP_201
+    resp.body = json.dumps({"success": True})
+    
+
 
 api = falcon.API()
 companies_endpoint = CompaniesResource()
